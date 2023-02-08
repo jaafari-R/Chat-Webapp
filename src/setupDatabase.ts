ad4d44a5bc616205f504cfc158a3eba0f1959350
potato <jaafariramadan@gmail.com>
2023-02-08 16:07:54 +0200

    Added Redis connection

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import Logger from 'bunyan';
 
 import { config } from '@root/config';
+import { redisConnection } from '@service/redis/redis.connection';
 
 const log: Logger = config.createLogger('Server');
 
@@ -11,6 +12,7 @@ export default () => {
             .connect(`${config.DATABASE_URL}`)
             .then(() => {
                 log.info('Successfully connected to database.');
+                redisConnection.connect();
             })
             .catch((error) => {
                 log.error('Failed to connect to database', error);
