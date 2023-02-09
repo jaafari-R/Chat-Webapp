8e66b36bef268e9c2dd8d29ed5e84066327794ac
potato <jaafariramadan@gmail.com>
2023-02-09 15:08:55 +0200

    Added JWT token to response on user creation

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -1,19 +1,22 @@
 import { ObjectId } from 'mongodb';
 import { Request, Response } from 'express';
+import { UploadApiResponse } from 'cloudinary';
+import HTTP_STATUS from 'http-status-codes';
+import { omit } from 'lodash';
+import JWT from 'jsonwebtoken';
+
+import { upload } from '@global/cloudinary-upload';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { UserCache } from '@service/redis/user.cache';
+import { authQueue } from '@service/queues/auth.queue';
+import { userQueue } from '@service/queues/user.queue';
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
 import { signupSchema } from '@auth/schemas/signup';
 import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
 import { authService } from '@service/db/auth.service';
 import { BadRequestError } from '@global/helpers/error-handler';
 import { Helpers } from '@global/helpers/helpers';
-import { UploadApiResponse } from 'cloudinary';
-import { upload } from '@global/cloudinary-upload';
-import HTTP_STATUS from 'http-status-codes';
-import { IUserDocument } from '@user/interfaces/user.interface';
-import { UserCache } from '@service/redis/user.cache';
-import { omit } from 'lodash';
-import { authQueue } from '@service/queues/auth.queue';
-import { userQueue } from '@service/queues/user.queue';
+import { config } from '@root/config';
 
 const userCache: UserCache = new UserCache();
 
@@ -38,12 +41,13 @@ export class SignUp {
             password,
             avatarColor
         });
+
         const result: UploadApiResponse = (await upload(avatarImage, `${userObjectId}`, true, true)) as UploadApiResponse;
         if (!result?.public_id) {
             throw new BadRequestError('Error: Failed to upload avatarImage.<br>' + result);
         }
 
-        // Add to redis user cache
+        // // Add to redis user cache
         const userDataForCache: IUserDocument = SignUp.prototype.userData(authData, userObjectId);
         userDataForCache.profilePicture = `https://res/cloudinary.com/dggusiuzi/image/upload/v${result.version}/${userObjectId}`;
         await userCache.saveUserToCache(`${userObjectId}`, uId, userDataForCache);
@@ -53,7 +57,24 @@ export class SignUp {
         authQueue.addAuthUserJob('addAuthUserToDB', { value: userDataForCache });
         userQueue.addUserJob('addUserToDB', { value: userDataForCache });
 
-        res.status(HTTP_STATUS.CREATED).json({ message: 'User created successfully!', authData });
+        const userJwt: string = SignUp.prototype.signToken(authData, userObjectId);
+        req.session = { jwt: userJwt };
+
+        res.status(HTTP_STATUS.CREATED).json({ message: 'User created successfully!', user: userDataForCache, token: userJwt });
+    }
+
+    private signToken(data: IAuthDocument, userObjectId: ObjectId): string
+    {
+        return JWT.sign(
+            {
+                userId: userObjectId,
+                uId: data.uId,
+                email: data.email,
+                username: data.username,
+                avatarColor: data.avatarColor
+            },
+            config.JWT_TOKEN!
+        );
     }
 
     private signupData(data: ISignUpData): IAuthDocument {
