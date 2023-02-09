166a414703695668d9416008a81771bc3267f1a4
potato <jaafariramadan@gmail.com>
2023-02-09 22:55:15 +0200

    Fixed Signup and Signin bugs

--- src/features/auth/controllers/signin.ts
+++ src/features/auth/controllers/signin.ts
@@ -8,7 +8,8 @@ import { authService } from '@service/db/auth.service';
 import { BadRequestError } from '@global/helpers/error-handler';
 import { signinSchema } from '@auth/schemas/signin';
 import { IAuthDocument } from '@auth/interfaces/auth.interface';
-import { userInfo } from 'os';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { userService } from '@service/db/user.service';
 
 export class SignIn
 {
@@ -17,27 +18,30 @@ export class SignIn
     {
         const { username, password } = req.body;
 
-        const user: IAuthDocument = await authService.getAuthUserByUsername(username);
-        if (!user) { // if user does not exist
+        const userAuth: IAuthDocument = await authService.getAuthUserByUsername(username);
+        if (!userAuth) { // if user does not exist
             throw new BadRequestError('Invalid credentials!');
         }
 
-        const passwordMatch: boolean = await user.comparePassword(password);
+        const passwordMatch: boolean = await userAuth.comparePassword(password);
 
         if (!passwordMatch) {
             throw new BadRequestError('Invalid credentials!');
         }
 
+        const user: IUserDocument = await userService.getUserByAuthId(`${userAuth._id}`);
+
         const userJwt: string = JWT.sign(
             {
-                userId: user.id,
-                uId: user.uId,
-                email: user.email,
-                username: user.username,
-                avatarColor: user.avatarColor
+                userId: user._id,
+                uId: userAuth.uId,
+                email: userAuth.email,
+                username: userAuth.username,
+                avatarColor: userAuth.avatarColor
             },
             config.JWT_TOKEN!
         );
+
         req.session = { jwt: userJwt };
         res.status(HTTP_STATUS.OK).json({ message: 'Successfully logged in!', user: user, token: userJwt });
     }
