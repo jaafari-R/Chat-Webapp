43de046c28b5cd6aac7cbd66fc69ad80eeafc2cc
potato <jaafariramadan@gmail.com>
2023-02-09 23:58:35 +0200

    Implemented DB getUserById method

--- src/shared/services/db/user.service.ts
+++ src/shared/services/db/user.service.ts
@@ -1,4 +1,4 @@
-import { IUserDocument } from '@user/interfaces/user.interface';
+import { IUser, IUserDocument } from '@user/interfaces/user.interface';
 import { UserModel } from '@user/models/user.schema';
 import mongoose from 'mongoose';
 
@@ -6,6 +6,17 @@ class UserService {
     public async createUser(data: IUserDocument): Promise<void>
     {
         await UserModel.create(data);
+    }
+
+    public async getUserById(userId: string): Promise<IUserDocument>
+    {
+        const users: IUserDocument[] = await UserModel.aggregate([
+            { $match: { _id: new mongoose.Types.ObjectId(userId) } },
+            { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+            { $unwind: '$authId' },
+            { $project: this.aggregateProject() }
+        ]);
+        return users[0];
     }
 
     public async getUserByAuthId(authId: string): Promise<IUserDocument>
