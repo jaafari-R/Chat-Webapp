528a78e4486b8b673f0ba2cc6e6dd8354cac56c2
potato <jaafariramadan@gmail.com>
2023-02-09 23:16:29 +0200

    Implemented Signout

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ const BASE_PATH = '/api/v1';
 export default (app: Application) => {
     const routes = () => {
         app.use('/queues', serverAdapter.getRouter());
+        app.use(BASE_PATH, authRoutes.signoutRoutes());
         app.use(BASE_PATH, authRoutes.routes());
     };
     routes();
